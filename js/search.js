a2a0dce601043f88e952ede62735dd13ee246f52
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-03-05 18:01:13 +0100

    Funciones de búsqueda de usuario añadidas en la barra superior

--- js/search.js
+++ js/search.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-  	$("#btn1").click(function(){
+  	$('#buscar-usuario').on('click',function(e){
+  		e.preventDefault(); // disable the default form submit event
+        var usuario = $("#input-usuario").val();
+
 		$.ajax({     
 			type: "GET",
-			url: 'https://api.github.com/search/users?q=Jose-Verdu-Diaz',
+			url: 'https://api.github.com/search/users?q='+usuario,
 			dataType: "json",
 			success: function (data) {
 				console.log(data);
-     		},
+				alert(data.items[0].login);
+     		}
 		});
 	});
 });
