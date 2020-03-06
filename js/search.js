e30bd05cc12dfc62a306b159deebbe9b583814c7
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-03-06 13:49:27 +0100

    Mejora: la página se modifica mediante plantillas html y jquery.

--- js/search.js
+++ js/search.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+
+	var PlantillaPanelInfo = $('#PlantillaPanelInfoOculta').html();
+
   	$('#buscar-usuario').on('click',function(e){
   		e.preventDefault(); // disable the default form submit event
         var usuario = $("#input-usuario").val();
         var n;
+
 		$.ajax({     
 			type: "GET",
 			url: 'https://api.github.com/search/users?q='+usuario,
@@ -10,9 +14,14 @@ $(document).ready(function(){
 			success: function (data) {
 				$(".panel-info").empty();
 				n = data.items.length;
+				console.log(data);
 				var i = 0;
 				while(i<n){
-					$("#accordion").append("<div class=\"card\"><div class=\"card-header\" id=\"Cabecera"+i+"\"><button class=\"btn btn-block\" data-toggle=\"collapse\" data-target=\"#collapse"+i+"\" aria-expanded=\"true\" aria-controls=\"collapse"+i+"\">"+data.items[i].login+"</button></div><div id=\"collapse"+i+"\" class=\"collapse\" aria-labelledby=\"headingOne\" data-parent=\"#accordion\"><div class=\"card-body\">TEST</div></div></div>");
+					$("#accordion").append(PlantillaPanelInfo);
+					$('#Cabecera').attr('id', 'Cabecera'+i);
+					$('#BotonColapsar').attr('id', 'BotonColapsar'+i);
+					$('#BotonColapsar'+i).attr('data-target', '#collapse'+i);
+					$('#collapse').attr('id', 'collapse'+i);		
 					i=i+1;
 				}
      		}
