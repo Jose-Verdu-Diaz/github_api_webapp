40003d0987ef7d770d746864ca575fe4b0af380c
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-03-06 12:13:05 +0100

    Los paneles se actualizan por cada búsqueda de usuario

--- js/search.js
+++ js/search.js
@@ -2,19 +2,21 @@ $(document).ready(function(){
   	$('#buscar-usuario').on('click',function(e){
   		e.preventDefault(); // disable the default form submit event
         var usuario = $("#input-usuario").val();
-
+        var n;
 		$.ajax({     
 			type: "GET",
 			url: 'https://api.github.com/search/users?q='+usuario,
 			dataType: "json",
 			success: function (data) {
-				console.log(data);
-				alert(data.items[0].login);
+				$(".panel-info").empty();
+				n = data.items.length;
+				var i = 0;
+				while(i<n){
+					$(".panel-info").append("<div><button type=\"button\" class=\"btn btn-info\" data-toggle=\"collapse\" data-target=\"#usuario"+i+"\">"+data.items[i].login+"</button><div id=\"usuario"+i+"\" class=\"collapse\">TEST</div></div>");
+					i=i+1;
+				}
      		}
 		});
-
-		//$(".panel-info").append("<div><button type="'button'" class="'btn btn-info'" data-toggle="'collapse'" data-target="'#demo'">Simple collapsible</button><div id="'demo'" class="'collapse'">TEST</div></div>");
-		$(".panel-info").append("<div><button type="'button'" class="'btn btn-info'" data-toggle="'collapse'" data-target="'#demo'">Simple collapsible</button></div>");
 	});	
 });
 
